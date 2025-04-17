552b1586472b175fa72b31c8512408f9f00db4fa
ZakDeveloperAI <belfakhirzakaria@gmail.com>
2025-04-17 19:10:36 +0200

    ai agent custom tools

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from dotenv import load_dotenv
+import json
 from pydantic import BaseModel
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent,AgentExecutor
+from tools import search_tool,wiki_tool,save_tool
 load_dotenv()
 
 class ResearchResponse(BaseModel):
@@ -30,8 +32,23 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 ).partial(format_instructions=parser.get_format_instructions())
 
+tools=[search_tool,wiki_tool]
 agent=create_tool_calling_agent(
     llm=llm,
     prompt=prompt,
-    tools=[]
+    tools=tools
 )
+
+agent_executor=AgentExecutor(agent=agent,tools=tools,verbose=True)
+#query=input("Enter your query: ")
+query="Hammer Head shark?"
+raw_response=agent_executor.invoke({"query":query})
+try:
+    output=raw_response.get('output').strip('`json').strip()
+    structured_response = parser.parse(output)
+    print(output)
+except Exception as e:
+    print(f"Error parsing response: {e}")
+
+
+print(structured_response.topic)
